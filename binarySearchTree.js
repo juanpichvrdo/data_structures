c84e966268e80bbe30785ff90a6cbceddd8c5af6
Juan Daniel Pichardo <jpichardopena@gmail.com>
2018-09-20 21:51:49 -0400

    Added contains method and a tree for testing

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -1,17 +1,42 @@
 class BST {
   constructor(value) {
-    this.value;
+    this.value = value;
     this.left = null;
     this.right = null;
   }
 
   insert(value) {
     if (value <= this.value) {
-      if (!this.left) this.left = new BTS(value);
+      if (!this.left) this.left = new BST(value);
       else this.left.insert(value);
     } else if (value > this.value) {
       if (!this.right) this.right = new BST(value);
       else this.right.insert(value);
     }
   }
+
+  contains(value) {
+    if (value === this.value) return true;
+    else if (value < this.value) {
+      if (!this.left) return false;
+      else return this.left.contains(value);
+    } else if (value > this.value) {
+      if (!this.right) return false;
+      else return this.right.contains(value);
+    }
+  }
 }
+
+const bst = new BST(50);
+
+bst.insert(30);
+bst.insert(70);
+bst.insert(100);
+bst.insert(60);
+bst.insert(59);
+bst.insert(20);
+bst.insert(45);
+bst.insert(35);
+bst.insert(85);
+bst.insert(105);
+bst.insert(10);
